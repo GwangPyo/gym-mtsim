fc72818196f5ce767bda29fa98cb9e7506e783a2
Gwangpyo Yoo <necrocathy@gmail.com>
2024-05-06 20:12:33 +0900

    addd time split max len

--- gym_mtsim/envs/mt_env.py
+++ gym_mtsim/envs/mt_env.py
@@ -35,7 +35,7 @@ class MtEnv(gym.Env):
             initial_balance_kwargs: Optional[Tuple[float, float]] = None,
             time_split: bool = False,
             min_time_split_length: int = 10,
-            max_time_limit: int = 500,
+            max_time_limit: int = 200,
             seed: int = 42,
     ) -> None:
         # validations
@@ -128,7 +128,7 @@ class MtEnv(gym.Env):
             min_time_split_length = self.min_time_split_length
             self._start_tick = self.np_rng.integers(self._initial_start_tick,
                                                     self._max_end_tick - min_time_split_length)
-            high = min(self._max_end_tick, self._start_tick + min_time_split_length + self.max_time_split_length)
+            high = min(self._max_end_tick, self._start_tick + self.max_time_split_length)
             self._end_tick = self.np_rng.integers(low=self._start_tick + min_time_split_length,
                                                   high=high)
             self._current_tick = self._start_tick
